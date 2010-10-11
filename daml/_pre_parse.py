32855e2e6c8d7dbb4f6f6bf264adbd0cf54267f6
Daniel Skinner <dasacc22@gmail.com>
2010-10-11 13:16:48 -0500

    started support for inlining tags and python statements

--- daml/_pre_parse.py
+++ daml/_pre_parse.py
@@ -36,6 +36,15 @@ def _pre_parse(f):
             offset -= 1
             continue
         
+        a = l.partition(' ')
+        if a[2] != '' and a[0][0] in ['%', '.', '#'] and (a[2][0] in ['%', '.', '#'] or a[2][-1] == ':'):
+            f.pop(i+offset)
+            f.insert(i+offset, ws+a[0])
+            offset += 1
+            ws += ' '
+            l = a[2]
+            f.insert(i+offset, ws+l)
+        
         # handle multiline function
         if mf is not None:
             if ws <= mf[0]:
@@ -97,7 +106,6 @@ def _pre_parse(f):
         if ':' in l:
             f.pop(i+offset)
             f.insert(i+offset, ws+l.replace(':', '\n '+ws))
-            print 'raargh!'
         
         # inspect for mixed content
         if l[-1] == ':':
