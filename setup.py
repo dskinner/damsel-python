d0581e87479b7901171ad7b7e6fba1e23e9d9c5a
Daniel Skinner <dasacc22@gmail.com>
2010-12-22 15:27:38 -0600

    updated setup.py for 0.1.4 release to pypi

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ from Cython.Distutils import build_ext
 
 setup(
     name='DAML',
-    version='0.1.3',
+    version='0.1.4',
     author='Daniel Skinner',
     author_email='dasacc22@gmail.com',
     url='http://daml.dasa.cc',
     license = "MIT License",
     packages = ["daml"],
     requires = ["lxml", "cython"],
-    description='Python implementation of HAML, extended.',
+    description='da Markup Language featuring html outlining via css-selectors, embedded python, and extensibility.',
     long_description = """\
 Features CSS selectors and indention for declaring page layout. Embed
 python in your documents such as functions, lambda's, variable declarations,
 for loops, list comprehensions, etc, writing it just as you would normally.
 Filters that are linked to python function calls. First use of this is a
 Django-style "block" and "extends". Will be easy to write custom filters and functions.
-Still under heavy development, lots of bugs. Parses your documents extremely fast!
+Still under heavy development, lots of bugs.
 Follow development at http://github.com/dasacc22/DAML
     """,
     classifiers = [
