585d08835ae0668b2b2dcb4996823628deb128d5
Daniel Skinner <dasacc22@gmail.com>
2010-12-22 14:32:12 -0600

    updated unittests

--- daml/test/test_basic.py
+++ daml/test/test_basic.py
@@ -7,11 +7,12 @@ import codecs
 class TestBasic(unittest.TestCase):
     def setUp(self):
         self.t = {
+            'basic_html': None,
             'basic_indent': None,
-            'basic_variable_indent': None,
-            'tag_hashes': None,
-            'multiline_text': None,
-            'basic_html': None}
+            'basic_inline': None,
+            'basic_tag_hashes': None,
+            'basic_variable_indent': None
+            }
 
         for k, v in self.t.items():
             # template file
@@ -20,28 +21,28 @@ class TestBasic(unittest.TestCase):
             b = open(os.path.join('', k+'.html')).read()
             self.t[k] = (a, b)
 
+    def test_basic_html(self):
+        parsed, expected = self.t['basic_html']
+        parsed = parse(parsed)
+        self.assertEqual(parsed.strip(), expected.strip())
+    
     def test_basic_indent(self):
         parsed, expected = self.t['basic_indent']
         parsed = parse(parsed)
         self.assertEqual(parsed.strip(), expected.strip())
-
+        
+    def test_basic_inline(self):
+        parsed, expected = self.t['basic_inline']
+        parsed = parse(parsed)
+        self.assertEqual(parsed.strip(), expected.strip())
+    
+    def test_basic_tag_hashes(self):
+        parsed, expected = self.t['basic_tag_hashes']
+        parsed = parse(parsed)
+        self.assertEqual(parsed.strip(), expected.strip())
+    
     def test_basic_variable_indent(self):
         parsed, expected = self.t['basic_variable_indent']
-        parsed = parse(parsed)
-        self.assertEqual(parsed.strip(), expected.strip())
-
-    def test_tag_hashes(self):
-        parsed, expected = self.t['tag_hashes']
-        parsed = parse(parsed)
-        self.assertEqual(parsed.strip(), expected.strip())
-
-    def test_multiline_text(self):
-        parsed, expected = self.t['multiline_text']
-        parsed = parse(parsed)
-        self.assertEqual(parsed.strip(), expected.strip())
-
-    def test_basic_html(self):
-        parsed, expected = self.t['basic_html']
         parsed = parse(parsed)
         self.assertEqual(parsed.strip(), expected.strip())
 
